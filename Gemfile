pauldacus/fat_free_crm

Added missing dependencies for meta_search and chosen

Gemfile
@@ -2,8 +2,11 @@ source 'http://rubygems.org'
 
 gem 'rails'
 gem 'prototype-rails'
+gem 'jquery-rails'
+gem 'ajax-chosen-rails'
 gem 'simple_form'
 gem 'will_paginate'
+gem 'meta_search'
 
 gemspec
 

dmytro-dobriev/crm-rubyonrails

Gemfile
@@ -1,6 +1,17 @@
 source 'http://rubygems.org'
 
+gem 'rails'
+gem 'prototype-rails'
+gem 'jquery-rails'
+gem 'ajax-chosen-rails'
+gem 'simple_form'
+gem 'will_paginate'
+gem 'ransack'
+
 gemspec
+
+gem 'paperclip'
+gem 'paper_trail'
 
 gem 'ruby-debug',   :platform => :mri_18
 gem 'ruby-debug19', :platform => :mri_19